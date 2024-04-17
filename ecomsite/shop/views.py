ffc7d5e5014d840c138738a765ef53706a58e4fc
Preet <preet16vaishnav@gmail.com>
2024-04-17 16:37:58 +0530

    Commit Six

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -27,13 +27,14 @@ def detail(request,id):
 
 def checkout(request):
     if request.method == "POST":
+        items = request.POST.get('items','')
         name = request.POST.get('name',"")
         email = request.POST.get('email',"")
         address = request.POST.get('address',"")
         city = request.POST.get('city',"")
         state = request.POST.get('state',"")
         zipcode = request.POST.get('zipcode',"")
-
-        order = Order(name=name,email=email,address=address,city=city,state=state,zipcode=zipcode)
+        total = request.POST.get("total","")
+        order = Order(name=name,items=items,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
         order.save()
     return render(request,'shop/checkout.html')
